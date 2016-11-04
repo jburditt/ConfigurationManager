0716a56ce2c20d1350a84d2780bf8b923c5a8534
Jebb Burditt <jebb.burditt@gmail.com>
2016-11-03 20:24:59 -0700

    working prototype

---
+++ ConfigurationManager.Tests/UnitTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Configuration;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConfigManager.Tests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            ConfigurationManager.RefreshSection("appSettings");
+            ConfigurationManager.RefreshSection("connectionStrings");
+        }
+
+        [TestMethod]
+        public void File_Does_Not_Exist()
+        {
+            Assert.IsFalse(ConfigManager.Load("C:\\asdf.xml"));
+        }
+
+        [TestMethod]
+        public void Invalid_XML()
+        {
+            var filePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent?.FullName + "\\Invalid.config";
+            Assert.IsFalse(ConfigManager.Load(filePath));
+        }
+
+        [TestMethod]
+        public void Load()
+        {
+            var filePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent?.FullName + "\\Local.config";
+            Assert.IsTrue(ConfigManager.Load(filePath));
+
+            Assert.AreEqual("local", ConfigurationManager.AppSettings["test1"]);
+            Assert.AreEqual("local", ConfigurationManager.AppSettings["test2"]);
+            Assert.AreEqual("app", ConfigurationManager.AppSettings["test3"]);
+
+            Assert.AreEqual("local", ConfigurationManager.ConnectionStrings["conn1"].ConnectionString);
+            Assert.AreEqual("app", ConfigurationManager.ConnectionStrings["conn2"].ConnectionString);
+            Assert.AreEqual("local", ConfigurationManager.ConnectionStrings["conn3"].ConnectionString);
+        }
+
+        [TestMethod]
+        public void Load_ActiveConfig()
+        {
+            var activeConfig = ConfigurationManager.AppSettings["BuildConfiguration"];
+            var filePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent?.FullName + $"\\Local.{activeConfig}.config";
+            Assert.IsTrue(ConfigManager.Load(filePath));
+
+            Assert.AreEqual("app", ConfigurationManager.AppSettings["test1"]);
+            Assert.AreEqual("app", ConfigurationManager.AppSettings["test2"]);
+            Assert.AreEqual("active", ConfigurationManager.AppSettings["test3"]);
+            Assert.AreEqual("active", ConfigurationManager.AppSettings["test4"]);
+        }
+    }
+}
---
+++ ConfigurationManager/ConfigManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace ConfigManager
+{
+    public class ConfigManager
+    {
+        public static void Initialize()
+        {
+            try
+            {
+                // active config
+                var activeConfig = ConfigurationManager.AppSettings["BuildConfiguration"];
+                string filePath;
+                if (!string.IsNullOrEmpty(activeConfig))
+                {
+                    filePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent?.FullName +
+                               $"\\Local.{activeConfig}.config";
+                    Load(filePath);
+                }
+
+                // global config
+                filePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent?.FullName + "\\Local.config";
+                Load(filePath);
+            } catch { }
+        }
+
+        public static bool Load(string filePath)
+        {
+            if (!File.Exists(filePath))
+                return false;
+
+            try
+            {
+                var config = XDocument.Load(filePath);
+
+                foreach (var element in config.Root.Element("appSettings").Elements("add"))
+                {
+                    var key = element.Attribute("key")?.Value;
+                    var value = element.Attribute("value")?.Value;
+
+                    if (key != null && value != null)
+                        ConfigurationManager.AppSettings[key] = value;
+                }
+
+                foreach (var element in config.Root.Element("connectionStrings").Elements("add"))
+                {
+                    var name = element.Attribute("name")?.Value;
+                    var connectionString = element.Attribute("connectionString")?.Value;
+                    var providerName = element.Attribute("providerName")?.Value;
+
+                    if (name != null && connectionString != null)
+                    {
+                        var conn = ConfigurationManager.ConnectionStrings[name];
+                        if (conn == null)
+                        {
+                            typeof (ConfigurationElementCollection).GetField("bReadOnly",
+                                BindingFlags.Instance | BindingFlags.NonPublic)?
+                                .SetValue(ConfigurationManager.ConnectionStrings, false);
+
+                            ConfigurationManager.ConnectionStrings.Add(
+                                new ConnectionStringSettings(name, connectionString, providerName));
+                        }
+                        else
+                        {
+                            typeof (ConfigurationElement).GetField("_bReadOnly",
+                                BindingFlags.Instance | BindingFlags.NonPublic)?.SetValue(conn, false);
+
+                            conn.ConnectionString = connectionString;
+                            if (providerName != null)
+                                conn.ProviderName = providerName;
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
