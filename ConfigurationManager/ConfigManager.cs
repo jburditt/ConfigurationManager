6912784bbd0fb2fa24e29b027d2e01e9e82bfaae
Jebb Burditt <jebb.burditt@gmail.com>
2017-03-27 16:01:52 -0700

    Update ConfigManager.cs

--- ConfigurationManager/ConfigManager.cs
+++ ConfigurationManager/ConfigManager.cs
@@ -1,11 +1,12 @@
-﻿using System;
+using System;
 using System.Configuration;
 using System.IO;
 using System.Reflection;
 using System.Xml.Linq;
 
-namespace ConfigManager
+namespace Arcteryx.Web
 {
+    // TODO Disable in prod. Security concern if someone had write access to Web folder (which would never happen)
     public class ConfigManager
     {
         public static void Initialize()
@@ -17,15 +18,31 @@ namespace ConfigManager
                 string filePath;
                 if (!string.IsNullOrEmpty(activeConfig))
                 {
-                    filePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent?.FullName +
-                               $"\\Local.{activeConfig}.config";
+                    filePath = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent?.FullName + $"\\Arcteryx.Web\\Local.{activeConfig}.config";
                     Load(filePath);
                 }
 
                 // global config
-                filePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent?.FullName + "\\Local.config";
+                filePath = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent?.FullName + "\\Arcteryx.Web\\Local.config";
                 Load(filePath);
-            } catch { }
+            }
+            catch
+            {
+                // swallow error these are not critical
+            }
+        }
+
+        public static string DebugHtml()
+        {
+            var builder = new System.Data.Common.DbConnectionStringBuilder();
+            builder.ConnectionString = ConfigurationManager.ConnectionStrings["ArcteryxRepository"].ConnectionString;
+
+            var html = $"<h3>Database</h3><br>Server = [{builder["Data Source"]}]<br>Database Name = {builder["Initial Catalog"]}<br><br><h3>AppSettings</h3><br>";
+
+            foreach (var appSetting in ConfigurationManager.AppSettings)
+                html += $"{appSetting} = {ConfigurationManager.AppSettings[appSetting+""]}<br>";
+
+            return html;
         }
 
         public static bool Load(string filePath)
@@ -37,44 +54,46 @@ namespace ConfigManager
             {
                 var config = XDocument.Load(filePath);
 
-                foreach (var element in config.Root.Element("appSettings").Elements("add"))
-                {
-                    var key = element.Attribute("key")?.Value;
-                    var value = element.Attribute("value")?.Value;
+                if (config?.Root == null)
+                    return false;
 
-                    if (key != null && value != null)
-                        ConfigurationManager.AppSettings[key] = value;
-                }
-
-                foreach (var element in config.Root.Element("connectionStrings").Elements("add"))
-                {
-                    var name = element.Attribute("name")?.Value;
-                    var connectionString = element.Attribute("connectionString")?.Value;
-                    var providerName = element.Attribute("providerName")?.Value;
-
-                    if (name != null && connectionString != null)
+                if (config.Root.Element("appSettings") != null)
+                    foreach (var element in config.Root.Element("appSettings").Elements("add"))
                     {
-                        var conn = ConfigurationManager.ConnectionStrings[name];
-                        if (conn == null)
-                        {
-                            typeof (ConfigurationElementCollection).GetField("bReadOnly",
-                                BindingFlags.Instance | BindingFlags.NonPublic)?
-                                .SetValue(ConfigurationManager.ConnectionStrings, false);
+                        var key = element.Attribute("key")?.Value;
+                        var value = element.Attribute("value")?.Value;
 
-                            ConfigurationManager.ConnectionStrings.Add(
-                                new ConnectionStringSettings(name, connectionString, providerName));
-                        }
-                        else
-                        {
-                            typeof (ConfigurationElement).GetField("_bReadOnly",
-                                BindingFlags.Instance | BindingFlags.NonPublic)?.SetValue(conn, false);
+                        if (key != null && value != null)
+                            ConfigurationManager.AppSettings[key] = value;
+                    }
 
-                            conn.ConnectionString = connectionString;
-                            if (providerName != null)
-                                conn.ProviderName = providerName;
+                if (config.Root.Element("connectionStrings") != null)
+                    foreach (var element in config.Root.Element("connectionStrings").Elements("add"))
+                    {
+                        var name = element.Attribute("name")?.Value;
+                        var connectionString = element.Attribute("connectionString")?.Value;
+                        var providerName = element.Attribute("providerName")?.Value;
+
+                        if (name != null && connectionString != null)
+                        {
+                            var conn = ConfigurationManager.ConnectionStrings[name];
+                            if (conn == null)
+                            {
+                                typeof(ConfigurationElementCollection).GetField("bReadOnly", BindingFlags.Instance | BindingFlags.NonPublic)?
+                                    .SetValue(ConfigurationManager.ConnectionStrings, false);
+
+                                ConfigurationManager.ConnectionStrings.Add(new ConnectionStringSettings(name, connectionString, providerName));
+                            }
+                            else
+                            {
+                                typeof(ConfigurationElement).GetField("_bReadOnly", BindingFlags.Instance | BindingFlags.NonPublic)?.SetValue(conn, false);
+
+                                conn.ConnectionString = connectionString;
+                                if (providerName != null)
+                                    conn.ProviderName = providerName;
+                            }
                         }
                     }
-                }
             }
             catch (Exception)
             {
